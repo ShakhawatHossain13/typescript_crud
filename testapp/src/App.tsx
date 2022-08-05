d868b9eeaa09405cc3e4db6332e06aea796bca94
ShakhawatHossain13 <72038014+ShakhawatHossain13@users.noreply.github.com>
2022-08-05 14:17:18 +0600

    Update App.tsx

--- testapp/src/App.tsx
+++ testapp/src/App.tsx
@@ -1,7 +1,11 @@
 import React, {useState} from 'react';
 import './App.css';
+import { ThemeProvider } from "@material-ui/core";
 import InputField from './components/InputField';
 import TodoList from './components/TodoList';
+import Home from './components/Home';
+import theme from "./theme";  
+import GridDesign from './components/GridDesign';
 import { Todo } from './model';
 
 const App: React.FC=()=> {
@@ -19,13 +23,17 @@ const App: React.FC=()=> {
   } 
 
   return (
-   <>
-   <div className='container'>   
-        <h1>Task</h1>
-        <InputField todo = {todo} setTodo = {setTodo} handleAdd={handleAdd}/>
-        <TodoList todos={todos} setTodos={setTodos} />
-     </div>
-   </>
+   <React.Fragment>
+     <div className='container'>   
+          <h1>Task</h1>
+          <InputField todo = {todo} setTodo = {setTodo} handleAdd={handleAdd}/>
+          <TodoList todos={todos} setTodos={setTodos} />
+       </div>
+        <ThemeProvider theme={theme}> 
+          <Home /> 
+          <GridDesign/> 
+      </ThemeProvider>
+   </React.Fragment>
   );
 }
 
