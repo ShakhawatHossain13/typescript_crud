4aaf4277953c14c554b766085117b67029bb77fd
ShakhawatHossain13 <72038014+ShakhawatHossain13@users.noreply.github.com>
2022-08-05 16:19:32 +0600

    Update App.tsx

--- testapp/src/App.tsx
+++ testapp/src/App.tsx
@@ -1,39 +1,14 @@
 import React, {useState} from 'react';
-import './App.css';
-import { ThemeProvider } from "@material-ui/core";
-import InputField from './components/InputField';
-import TodoList from './components/TodoList';
-import Home from './components/Home';
-import theme from "./theme";  
+import './App.css'; 
+import TodoApp from './components/TodoApp'; 
 import GridDesign from './components/GridDesign';
-import { Todo } from './model';
 
-const App: React.FC=()=> {
-  const [todo, setTodo] = useState<string>("");
-  const[todos, setTodos] = useState<Todo[]>([]);
-
-  const handleAdd =(e: React.FormEvent)=>{
-    e.preventDefault();
-    if(todo){
-      setTodos(
-          [...todos, {id : Date.now(), todo, isDone: false}]        
-        );
-      setTodo("");
-    }
-  } 
-
+const App: React.FC=()=> { 
   return (
-   <React.Fragment>
-     <div className='container'>   
-          <h1>Task</h1>
-          <InputField todo = {todo} setTodo = {setTodo} handleAdd={handleAdd}/>
-          <TodoList todos={todos} setTodos={setTodos} />
-       </div>
-        <ThemeProvider theme={theme}> 
-          <Home /> 
-          <GridDesign/> 
-      </ThemeProvider>
-   </React.Fragment>
+    <React.Fragment>
+      <TodoApp/>
+      <GridDesign/>
+    </React.Fragment>    
   );
 }
 
